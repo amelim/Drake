002db2ea7eb3082241cabe90efe2c2dfeaf7aefa
Andrew Melim <Andrew.Melim@gatech.edu>
2013-10-01 13:08:27 -0400

    Renamed lgiths to FOV

--- FOV.lua
+++ FOV.lua
@@ -1,5 +1,9 @@
 --FOV.lua
---Function to compute FOV
+--Lighting based on Monoco lighting engine
+--https://www.facebook.com/notes/monaco/line-of-sight-in-a-tile-based-world/411301481995
 
-function FOV(x,y,floor)
-end
+--construct a big list of all "forward facing" edges
+
+
+--Next, I link up each of these edges. So now each tile-length edge knows about its neighbors: 
+--each has a "next" and a "previous" edge. Some edges are dead-ends, though: they don't have a next or a previous.
